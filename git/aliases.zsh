637887f12e72d8e721f0b6895b9260745ad31d0a
geehaws <georgehaws@Georges-MacBook-Pro.local>
2014-11-12 15:43:32 -0800

    shorter diff alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,5 +18,5 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gdif='git diff --cached'
+alias gif='git diff --cached'
 alias gad='git add'
