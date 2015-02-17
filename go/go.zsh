599d3f4e6306d23e55c4d71ced3252b2d27adb5e
George Haws <george@apcera.com>
2015-02-17 10:48:49 -0800

    include go config

---
+++ go/go.zsh
@@ -0,0 +1,5 @@
+export PATH="/usr/local/bin:$PATH"
+export GOPATH="$HOME/go"
+export BUILDROOT="$HOME/go"
+export PATH="$PATH:$BUILDROOT/bin"
+export PKG_CONFIG_PATH=$BUILDROOT/bin/openssl/lib/pkgconfig:$PKG_CONFIG_PATH
